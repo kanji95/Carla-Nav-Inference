4f5c3ac0324fc6ae3942a4b1909f3137bd045072
ashwin-mittal <ashwin.mittal2553@gmail.com>
2022-02-14 17:29:13 +0530

    Add data loader

--- dataloader.py
+++ dataloader.py
@@ -1,24 +1,63 @@
 import os
-import numpy as np
 from glob import glob
+
+import numpy as np
+from PIL import Image
 from torch.utils.data import Dataset
-        
+
+
 class CarlaDataset(Dataset):
     """Some Information about CarlaDataset"""
-    def __init__(self, args, data_root="/ssd_scratch/cvit/kanishk/carla_data", split="train"):
-        super(CarlaDataset, self).__init__()
-        
-        self.split = split
-        
-        self.data_root = data_root
-        self.img_dir = os.path.join(self.data_dir, "images")
-        self.mask_dir = os.path.join(self.data_dir, "annotations")
-        self.trans_dir = os.path.join(self.data_dir, "inverse_matrix")
-        
-        
 
-    def __getitem__(self, index):
-        return 
+    def __init__(
+        self,
+        args,
+        data_root="/ssd_scratch/cvit/kanishk/carla_data",
+        split="train",
+        transform=None,
+        dataset_len=10000,
+        skip=10,
+    ):
+        self.data_dir = os.path.join(data_root, split)
+        self.transform = transform
+        self.dataset_len = dataset_len
+        self.skip = skip
+        self.episodes = sorted(os.listdir(self.data_dir))
 
     def __len__(self):
-        return 
+        return self.dataset_len
+
+    def __getitem__(self, idx):
+        output = {}
+
+        episode_dir = os.path.join(self.data_dir, np.random.choice(self.episodes)[0])
+
+        image_files = sorted(glob(episode_dir + f"/images/*.png"))
+        matrix_files = sorted(glob(episode_dir + f"/inverse_matrix/*.npy"))
+        position_file = os.path.join(episode_dir, "vehicle_positions.txt")
+        command_path = os.path.join(episode_dir, "command.txt")
+
+        num_files = len(image_files)
+
+        vehicle_positions = []
+        with open(position_file, "rb") as fhand:
+            for line in fhand:
+                position = np.array(line.split(b","), dtype=np.float32)
+                vehicle_positions.append(position)
+
+        sample_idx = np.random.choice(range(self.skip, num_files - self.skip))
+
+        img_path = image_files[sample_idx]
+        with open(img_path, "rb") as f:
+            img = Image.open(f).convert("RGB")
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        output["image"] = np.array(img)
+        output["command"] = open(command_path, "rb").read()
+        output["vehicle_position"] = vehicle_positions[sample_idx]
+        output["matrix"] = np.load(matrix_files[sample_idx])
+        output["next_vehicle_position"] = vehicle_positions[sample_idx + 1]
+
+        return output
