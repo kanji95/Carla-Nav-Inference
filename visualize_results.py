6f4fb1eb2e8792904a6b57d2b7e324abdb4e4fd1
kanji95 <kanishk5991@gmail.com>
2022-02-17 15:22:30 +0530

    logging video results

--- visualize_results.py
+++ visualize_results.py
@@ -17,7 +17,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 num_gpu = torch.cuda.device_count()
 print(f"Using {device} with {num_gpu} GPUS!")
 
-experiment = wandb.init(project="Language Navigation")
+experiment = wandb.init(project="Language Navigation", dir="/tmp")
 
 val_path = "/ssd_scratch/cvit/kanishk/carla_data/val/"
 glove_path = "/ssd_scratch/cvit/kanishk/glove"
@@ -29,7 +29,7 @@ return_layers = {"layer2": "layer2", "layer3": "layer3", "layer4": "layer4"}
 img_backbone = torch.hub.load("facebookresearch/dino:main", "dino_resnet50")
 visual_encoder = IntermediateLayerGetter(img_backbone, return_layers)
 
-network = IROSBaseline(visual_encoder, hidden_dim=256, mask_dim=112)
+network = IROSBaseline(visual_encoder, hidden_dim=256, mask_dim=448)
 
 if num_gpu > 1:
     network = nn.DataParallel(network)
@@ -59,14 +59,18 @@ mask_transform = transforms.Compose(
 
 frame_mask = torch.ones(1, 14 * 14, dtype=torch.int64).cuda(non_blocking=True)
 
+columns = ["Command"]
+
 episodes = glob(val_path + "*")
 for episode in episodes:
 
-    command_path = episode + "command.txt"
+    command_path = episode + "/command.txt"
     command = open(command_path, "r").read()
     command = re.sub(r"[^\w\s]", "", command)
 
     phrase, phrase_mask = corpus.tokenize(command)
+    phrase = phrase.unsqueeze(0)
+    phrase_mask = phrase_mask.unsqueeze(0)
 
     image_files = sorted(glob(episode + "/images/*.png"))
     mask_files = sorted(glob(episode + "/masks/*.png"))
@@ -77,11 +81,11 @@ for episode in episodes:
 
     for image_file, mask_file in zip(image_files, mask_files):
 
-        image = Image.open(image_file)
-        gt_mask = Image.open(mask_file)
+        image = Image.open(image_file).convert("RGB")
+        gt_mask = Image.open(mask_file).convert("L")
 
-        frame = img_transform(image).cuda(non_blocking=True)
-        gt_mask = mask_transform(gt_mask).cuda(non_blocking=True)
+        frame = img_transform(image).cuda(non_blocking=True).unsqueeze(0)
+        gt_mask = mask_transform(gt_mask).cuda(non_blocking=True).unsqueeze(0)
 
         mask = network(frame, phrase, frame_mask, phrase_mask)
 
@@ -89,13 +93,29 @@ for episode in episodes:
         mask_video.append(mask.detach().cpu().numpy())
         gt_mask_video.append(gt_mask.detach().cpu().numpy())
 
-    frame_video = np.stack(frame_video, axis=0)
-    mask_video = np.stack(mask_video, axis=0)
-    gt_mask_video = np.stack(gt_mask_video, axis=0)
+    frame_video = np.concatenate(frame_video, axis=0)
+    mask_video = np.concatenate(mask_video, axis=0)
+    gt_mask_video = np.concatenate(gt_mask_video, axis=0)
+
+    # import pdb; pdb.set_trace()
+    mask_video_overlay = np.copy(frame_video)
+    mask_video_overlay[:, 0] += (mask_video[:, 0]/mask_video.max())
+    mask_video_overlay = np.clip(mask_video_overlay, a_min=0., a_max=1.)
+
+    gt_mask_video_overlay = np.copy(frame_video)
+    gt_mask_video_overlay[:, 0] += gt_mask_video[:, 0]
+    gt_mask_video_overlay = np.clip(gt_mask_video_overlay, a_min=0., a_max=1.)
+
+    frame_video = np.uint8(frame_video * 255)
+    mask_video = np.uint8(mask_video_overlay * 255)
+    gt_mask_video = np.uint8(gt_mask_video_overlay * 255)
+
+    print(command)
 
     wandb.log(
-        {"video": wandb.Video(frame_video, fps=4, format="gif")},
-        {"pred_mask": wandb.Video(mask_video, fps=4, format="gif")},
-        {"gt_mask": wandb.Video(gt_mask_video, fps=4, format="gif")},
-        {"command": command},
+        {
+            "video": wandb.Video(frame_video, fps=4, caption=command, format="gif"),
+            "pred_mask": wandb.Video(mask_video, fps=4, format="gif"),
+            "gt_mask": wandb.Video(gt_mask_video, fps=4, format="gif"),
+        }
     )
