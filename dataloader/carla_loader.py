84679eba3480df3ad105d73a40d00433a366be9d
kanji95 <kanishk5991@gmail.com>
2022-03-11 17:42:22 +0530

    resolve dataloader issue

--- dataloader/carla_loader.py
+++ dataloader/carla_loader.py
@@ -21,12 +21,13 @@ from einops import repeat
 from .word_utils import Corpus
 
 IGNORE = {
-    # "train": ['107', '138', '164', '170', '175', '193', '211', '213', '219', '227', '238', '248', '250', '254', '260', '270', '28', '283', '284', '285', '288', '293', '298', '305', '52', '80'], 
+    # "train": ['107', '138', '164', '170', '175', '193', '211', '213', '219', '227', '238', '248', '250', '254', '260', '270', '28', '283', '284', '285', '288', '293', '298', '305', '52', '80'],
     # "val": ['1', '44']
     # "train": ['100', '128', '153', '159', '163', '180', '197', '198', '204', '211', '221', '231', '233', '237', '242', '251', '263', '264', '265', '268', '27', '273', '278', '284', '286', '327', '50', '65', '75'],
-    "train" :['65', '100', '128', '153', '159', '163', '180', '197', '198', '204', '211', '221', '231', '233', '237', '242', '251', '263', '264', '265', '268', '27', '273', '278', '284', '286', '327', '356', '357', '366', '374', '390', '400', '402', '406', '413', '415', '429', '430', '437', '50', '75'],
+    "train": ['65', '100', '128', '153', '159', '163', '180', '197', '198', '204', '211', '221', '231', '233', '237', '242', '251', '263', '264', '265', '268', '27', '273', '278', '284', '286', '327', '356', '357', '366', '374', '390', '400', '402', '406', '413', '415', '429', '430', '437', '50', '75'],
     "val": []
 }
+
 
 def world_to_pixel(K, rgb_matrix, destination,  curr_position):
     point_3d = np.ones((4, destination.shape[1]))
@@ -57,7 +58,7 @@ def world_to_pixel(K, rgb_matrix, destination,  curr_position):
 
 
 def get_curve_length(points):
-    return np.sum([np.linalg.norm(points[i + 1] - points[i])  for i in range(len(points) - 1)])
+    return np.sum([np.linalg.norm(points[i + 1] - points[i]) for i in range(len(points) - 1)])
 
 
 class CarlaDataset(Dataset):
@@ -74,7 +75,7 @@ class CarlaDataset(Dataset):
         skip=5,
         sequence_len=16,
         mode="image",
-        image_dim=224, 
+        image_dim=224,
         mask_dim=112
     ):
         self.data_dir = os.path.join(data_root, split)
@@ -86,7 +87,7 @@ class CarlaDataset(Dataset):
         self.skip = skip
         self.sequence_len = sequence_len
         self.mode = mode
-        
+
         self.image_dim = image_dim
         self.mask_dim = mask_dim
 
@@ -95,8 +96,8 @@ class CarlaDataset(Dataset):
 
         self.episodes = sorted(os.listdir(self.data_dir))
         print("Number of episodes before removal: ", len(self.episodes))
-        
-        ## Remove Episodes
+
+        # Remove Episodes
         for episode in IGNORE[split]:
             self.episodes.remove(episode)
         print("Number of episodes after removal: ", len(self.episodes))
@@ -158,7 +159,8 @@ class CarlaDataset(Dataset):
     def __getitem__(self, idx):
         output = {}
 
-        episode_dir = os.path.join(self.data_dir, np.random.choice(self.episodes))
+        episode_dir = os.path.join(
+            self.data_dir, np.random.choice(self.episodes))
 
         image_files = sorted(glob(episode_dir + f"/images/*.png"))
         mask_files = sorted(glob(episode_dir + f"/masks/*.png"))
@@ -207,12 +209,12 @@ class CarlaFullDataset(Dataset):
         skip=5,
         sequence_len=16,
         mode="image",
-        image_dim=224, 
+        one_in_n=1,
+        image_dim=224,
         mask_dim=112,
         traj_dim=56,
         traj_frames=10,
         traj_size=25,
-        skip_frame=2,
     ):
         self.data_dir = os.path.join(data_root, split)
 
@@ -224,23 +226,22 @@ class CarlaFullDataset(Dataset):
         self.skip = skip
         self.sequence_len = sequence_len
         self.mode = mode
-        
+        self.one_in_n = one_in_n
+
         self.image_dim = image_dim
         self.mask_dim = mask_dim
         self.traj_dim = traj_dim
-        
+
         self.traj_frames = traj_frames
         self.traj_size = traj_size
-        
-        self.skip_frame = skip_frame
 
         if self.mode == "video":
             self.dataset_len = self.dataset_len//self.sequence_len
 
         self.episodes = sorted(os.listdir(self.data_dir))
         print("Number of episodes before removal: ", len(self.episodes))
-        
-        ## Remove Episodes
+
+        # Remove Episodes
         for episode in IGNORE[split]:
             self.episodes.remove(episode)
         print("Number of episodes after removal: ", len(self.episodes))
@@ -252,11 +253,11 @@ class CarlaFullDataset(Dataset):
 
     # TODO - Include Vehicle Position
     def get_video_data(self, K, image_files, mask_files, matrix_files, vehicle_positions, target_positions, T=10):
-        
+
         num_files = len(image_files)
-        
+
         sample_idx = np.random.choice(range(num_files - self.skip - T))
-        
+
         prev_idx = sample_idx
         while True:
             rgb_matrix = np.load(matrix_files[sample_idx])
@@ -275,27 +276,36 @@ class CarlaFullDataset(Dataset):
             if prev_idx == sample_idx:
                 print("remove ", image_files)
                 break
-        
+
         frames = []
         frame_masks = []
         orig_frames = []
-        
-        if sample_idx < self.sequence_len:
-            indices = [0]*(self.sequence_len - sample_idx)
-            indices.extend(list(range(1, sample_idx + 1)))
-            
-            start_idx = 0
+
+        valid_indices = list(range(0, sample_idx, self.one_in_n))
+        if len(valid_indices) > self.sequence_len:
+            indices = valid_indices[-self.sequence_len:]
         else:
-            indices = list(range(self.sequence_len))
-            start_idx = sample_idx - self.sequence_len + 1
+            indices = [0] * (self.sequence_len - len(valid_indices)) + valid_indices
+        start_idx = indices[0]
+        # if sample_idx < self.sequence_len:
+        #     indices = [0]*(self.sequence_len - sample_idx)
+        #     indices.extend(
+        #         list(range(1, sample_idx * self.one_in_n + 1, self.one_in_n)))
+        #     start_idx = 0
+        # else:
+        #     indices = list(
+        #         range(0, self.sequence_len*self.one_in_n, self.one_in_n))
+        #     start_idx = sample_idx - self.sequence_len * self.one_in_n + 1
 
         final_click_idx = target_positions['click_no'].max()
-        
+
         for index in indices:
-            img_path = image_files[start_idx + index]
-            mask_path = mask_files[start_idx + index]
-            
-            curr_click_idx = target_positions.iloc[sample_idx + index].to_list()[-1]
+            # print(target_positions.shape, sample_idx, index)
+            img_path = image_files[index]
+            mask_path = mask_files[index]
+
+            # print(target_positions.shape, sample_idx, index)
+            curr_click_idx = target_positions.iloc[index].to_list()[-1]
 
             img = Image.open(img_path).convert("RGB")
             mask = Image.open(mask_path).convert("L")
@@ -304,19 +314,19 @@ class CarlaFullDataset(Dataset):
 
             if self.img_transform:
                 img = self.img_transform(img)
-            
+
             if self.mask_transform:
                 mask = self.mask_transform(mask)
             mask[mask > 0] = 1
-            
+
             mask_ = torch.zeros_like(mask)
             mask_ = repeat(mask_, "c h w -> (repeat c) h w", repeat=2)
-            
+
             if curr_click_idx == final_click_idx:
                 mask_[1] = mask[0]
             else:
                 mask_[0] = mask[0]
-            
+
             mask = mask_ + 1e-4
 
             frames.append(img)
@@ -324,11 +334,10 @@ class CarlaFullDataset(Dataset):
 
         orig_frames = np.stack(orig_frames, axis=0)
         frames = torch.stack(frames, dim=1)
-        # frame_masks = torch.stack(frame_masks, dim=1)
-        frame_masks = frame_masks[-1]
-        
+        frame_masks = torch.stack(frame_masks, dim=1)
+
         rgb_matrix = np.load(matrix_files[sample_idx])
-        
+
         pixel_coordinates = [np.array([0, 0])]
         position_0 = vehicle_positions[sample_idx]
         position_0 = np.array(position_0).reshape(-1, 1)
@@ -336,7 +345,7 @@ class CarlaFullDataset(Dataset):
         for t in range(num_files - sample_idx - 1):
             position_t = vehicle_positions[sample_idx + t]
             position_t = np.array(position_t).reshape(-1, 1)
-            
+
             pixel_t_2d = world_to_pixel(K, rgb_matrix, position_t, position_0)
 
             if pixel_t_2d.shape[-1] == 0:
@@ -355,25 +364,26 @@ class CarlaFullDataset(Dataset):
 
             if len(pixel_coordinates) > T:
                 break
-        
+
         pixel_coordinates = np.vstack(pixel_coordinates[1:])[:, None]
-        
+
         traj_mask = np.zeros((orig_frames.shape[1], orig_frames.shape[2]))
-        traj_mask = cv2.polylines(traj_mask, [pixel_coordinates], isClosed=False, color=(255), thickness=self.traj_size)
+        traj_mask = cv2.polylines(traj_mask, [pixel_coordinates], isClosed=False, color=(
+            255), thickness=self.traj_size)
         traj_mask = Image.fromarray(traj_mask)
         traj_mask = self.traj_transform(traj_mask)
         traj_mask[traj_mask > 0] = 1
-        
+
         # print(traj_mask.min(), traj_mask.max(), pixel_coordinates.shape)
         # print(traj_mask.shape, orig_frames.shape)
         return frames, orig_frames, frame_masks, traj_mask, sample_idx
-    
+
     def get_image_data(self, K, image_files, mask_files, matrix_files, vehicle_positions, target_positions, T=10):
-        
+
         num_files = len(image_files)
-        
+
         sample_idx = np.random.choice(range(num_files - self.skip - T))
-        
+
         prev_idx = sample_idx
         while True:
             rgb_matrix = np.load(matrix_files[sample_idx])
@@ -392,12 +402,12 @@ class CarlaFullDataset(Dataset):
             if prev_idx == sample_idx:
                 print("remove ", image_files)
                 break
-        
+
         img_path = image_files[sample_idx]
         mask_path = mask_files[sample_idx]
         img = Image.open(img_path).convert("RGB")
         mask = Image.open(mask_path).convert("L")
-        
+
         final_click_idx = target_positions['click_no'].max()
         curr_click_idx = target_positions.iloc[sample_idx].to_list()[-1]
 
@@ -409,27 +419,26 @@ class CarlaFullDataset(Dataset):
         if self.mask_transform:
             mask = self.mask_transform(mask)
             mask[mask > 0] = 1
-            
+
         mask_ = torch.zeros_like(mask)
         mask_ = repeat(mask_, "c h w -> (repeat c) h w", repeat=2)
-        
+
         if curr_click_idx == final_click_idx:
             mask_[1] = mask[0]
         else:
-            mask_[0] = mask[0]    
+            mask_[0] = mask[0]
         mask = mask_ + 1e-4
-        
+
         rgb_matrix = np.load(matrix_files[sample_idx])
-        
+
         pixel_coordinates = [np.array([0, 0])]
         position_0 = vehicle_positions[sample_idx]
         position_0 = np.array(position_0).reshape(-1, 1)
-        
 
         for t in range(num_files - sample_idx - 1):
             position_t = vehicle_positions[sample_idx + t]
             position_t = np.array(position_t).reshape(-1, 1)
-            
+
             pixel_t_2d = world_to_pixel(K, rgb_matrix, position_t, position_0)
 
             if pixel_t_2d.shape[-1] == 0:
@@ -448,11 +457,12 @@ class CarlaFullDataset(Dataset):
 
             if len(pixel_coordinates) > T:
                 break
-        
+
         pixel_coordinates = np.vstack(pixel_coordinates[1:])[:, None]
-        
+
         traj_mask = np.zeros((orig_image.shape[0], orig_image.shape[1]))
-        traj_mask = cv2.polylines(traj_mask, [pixel_coordinates], isClosed=False, color=(255), thickness=self.traj_size)
+        traj_mask = cv2.polylines(traj_mask, [pixel_coordinates], isClosed=False, color=(
+            255), thickness=self.traj_size)
         traj_mask = Image.fromarray(traj_mask)
         traj_mask = self.traj_transform(traj_mask)
         traj_mask[traj_mask > 0] = 1
@@ -462,7 +472,8 @@ class CarlaFullDataset(Dataset):
     def __getitem__(self, idx):
         output = {}
 
-        episode_dir = os.path.join(self.data_dir, np.random.choice(self.episodes))
+        episode_dir = os.path.join(
+            self.data_dir, np.random.choice(self.episodes))
 
         image_files = sorted(glob(episode_dir + f"/images/*.png"))
         mask_files = sorted(glob(episode_dir + f"/masks/*.png"))
@@ -479,9 +490,10 @@ class CarlaFullDataset(Dataset):
             for line in fhand:
                 position = np.array(line.split(","), dtype=np.float32)
                 vehicle_positions.append(position)
-        
-        target_positions = pd.read_csv(target_path, names=['x', 'y', 'z', 'click_no'])
-            
+
+        target_positions = pd.read_csv(
+            target_path, names=['x', 'y', 'z', 'click_no'])
+
         traj_mask = None
         sample_idx = None
         if self.mode == "image":
