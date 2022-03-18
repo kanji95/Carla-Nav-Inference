533a45ea28e56e56cd8861ec4d2492739541ae03
kanji95 <kanishk5991@gmail.com>
2022-03-18 14:59:05 +0530

    const parsing

--- extract_span.py
+++ extract_span.py
@@ -19,7 +19,14 @@ command_files = glob(data_root + split + "/*/command.txt")
 predictor = Predictor.from_path(
     "https://storage.googleapis.com/allennlp-public-models/elmo-constituency-parser-2020.02.10.tar.gz"
 )
-# predictor.eval()
+
+def check_validity(constituents_):
+    word_list = [constituent_['word'] for constituent_ in constituents_ if constituent_["nodeType"] != "CC"]
+    count_list = [len(word.split(" ")) for word in word_list]
+    if 1 in count_list:
+        return False
+    else:
+        return True
 
 result = {}
 for command_file in command_files:
@@ -28,23 +35,32 @@ for command_file in command_files:
     if command[-1] == ".":
         command = command[:-1]
     episode = command_file.split("/")[-2]
-    # print(command, episode)
 
     predictions = predictor.predict(sentence=command)
     constituents = predictions["hierplane_tree"]["root"]["children"]  # [0]['children']
-
-    if len(constituents) == 1 and "children" in constituents[0]:
-        constituents = constituents[0]["children"]
-    # print(constituents)
-    sub_commands = [
-        constituent["word"]
-        for constituent in constituents
-        if constituent["nodeType"] != "CC"
-    ]
-    # print(sub_commands)
+    if not check_validity(constituents):
+        # constituents = predictions["hierplane_tree"]["root"]
+        sub_commands = [command]
+    else:
+        if len(constituents) == 1 and "children" in constituents[0]:
+            constituents_ = constituents[0]["children"]
+            if not check_validity(constituents_):
+                pass
+            else:
+                constituents = constituents_
+            # word_list = [constituent_['word'] for constituent_ in constituents_ if constituent_["nodeType"] != "CC"]
+            # count_list = [len(word.split(" ")) for word in word_list]
+            # if 1 in count_list:
+            #     pass
+            # else:
+            #     constituents = constituents_
+        sub_commands = [
+            constituent["word"]
+            for constituent in constituents
+            if constituent["nodeType"] != "CC"
+        ]
     result[int(episode)] = {"command": command, "sub_command": sub_commands}
 
-# import pdb; pdb.set_trace()
 df1 = pd.DataFrame.from_dict(result, orient="index")
 df2 = pd.DataFrame(df1["sub_command"].to_list(), index=df1.index)
 df = pd.concat([df1, df2], axis=1)
