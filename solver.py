93a2592895fd4ad2bb751897fbaf46e17216872c
kanji95 <kanishk5991@gmail.com>
2022-03-22 06:17:35 +0530

    bin class

--- solver.py
+++ solver.py
@@ -167,8 +167,8 @@ class Solver(object):
                 transforms.Resize((self.image_dim, self.image_dim)),
                 transforms.RandomGrayscale(p=0.3),
                 transforms.ToTensor(),
-                # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
-                #                      0.229, 0.224, 0.225]),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                                     0.229, 0.224, 0.225]),
             ]
         )
 
@@ -176,8 +176,8 @@ class Solver(object):
             [
                 transforms.Resize((self.image_dim, self.image_dim)),
                 transforms.ToTensor(),
-                # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
-                #                      0.229, 0.224, 0.225]),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                                     0.229, 0.224, 0.225]),
             ]
         )
 
