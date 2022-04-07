91fb8a7516137697582ba7b134d8384d3251625b
Varun Chhangani <varunchhangani@gmail.com>
2022-04-07 22:41:10 +0530

    infer vit

--- automate_inference.py
+++ automate_inference.py
@@ -1,7 +1,7 @@
 import os
 # [4, 37, 38, 39 ,40, 42, 45, 46] conv3d
 episodes = list(range(0, 50))
-checkpoint = './saved_model/conv3d_baseline_class_level_combo_multi_head_hd_384_sf_10_tf_20_05_Apr_09_00.pth'
+checkpoint = './saved_model/vit_small_patch16_224_class_level_combo_multi_head_hd_384_sf_10_tf_20_05_Apr_09_01.pth'
 maps = ['Town10HD', 'Town03',
         'Town05', 'Town10HD',
         'Town01', 'Town05',
@@ -29,7 +29,7 @@ maps = ['Town10HD', 'Town03',
         'Town05', 'Town01']
 command = True
 for i in range(len(episodes)):
-    os.system(f"python inference_model.py --img_backbone conv3d_baseline --hidden_dim 384 --image_dim 224 --mask_dim 224 --traj_dim 224 --sync --threshold 0.00005 \
+    os.system(f"python inference_model.py --img_backbone vit_small_patch16_224 --hidden_dim 384 --image_dim 224 --mask_dim 224 --traj_dim 224 --sync --threshold 0.00005 \
         --checkpoint {checkpoint} --glove_path E:/carla/carla/CARLA_0.9.12/glove/glove/ --target mask \
             --num_frames 4 --traj_frames 20 --attn_type multi_head --one_in_n 10\
             --sampling 5 --stop_criteria confidence --confidence 130 --min_confidence 35 --distance 5 --map {maps[episodes[i]]}\
