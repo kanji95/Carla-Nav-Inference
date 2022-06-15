e96eaa5bac2576f933a806fced1dbb78f7850f3c
Varun Chhangani <varunchhangani@gmail.com>
2022-06-15 20:20:19 +0530

    dataset 08

--- automate_inference_val.py
+++ automate_inference_val.py
@@ -1,6 +1,7 @@
 import os
-episodes = list(range(0, 25))
-checkpoint = './saved_model/clip_ViT-B_32_class_level_combo_multi_head_hd_512_sf_10-8_tf_10_12_May_05_13.pth'
+episodes = [15,  22]+list(range(25, 25))
+checkpoint = './saved_model/nomap_clip/clip_ViT-B_32_class_level_combo_multi_head_hd_512_sf_10-6_tf_20_0.20166.pth'
+
 maps = ['Town05', 'Town05', 'Town01', 'Town02', 'Town05', 'Town05',
         'Town03', 'Town10HD', 'Town02', 'Town05', 'Town05', 'Town10HD',
         'Town01', 'Town10HD', 'Town01', 'Town03', 'Town10HD', 'Town07',
@@ -11,6 +12,6 @@ command = True
 for i in range(len(episodes)):
     os.system(f"python inference_model.py --img_backbone clip_ViT-B/32 --hidden_dim 512 --image_dim 224 --mask_dim 224 --traj_dim 224 --sync --threshold 0.00005 \
         --checkpoint {checkpoint} --glove_path E:/carla/carla/CARLA_0.9.12/glove/glove/ --target mask \
-            --num_frames 8 --traj_frames 10 --attn_type multi_head --one_in_n 10\
-            --sampling 5 --stop_criteria confidence --confidence 200 --min_confidence 50 --infer_dataset val --distance 5 --map {maps[episodes[i]]}\
-                 --num_preds 5 --spawn {episodes[i]} {'--command' if command else ''}")
+            --num_frames 6 --traj_frames 20 --attn_type multi_head --one_in_n 10\
+            --sampling 5 --stop_criteria confidence --confidence 100 --min_confidence 20 --infer_dataset val --distance 5 --map {maps[episodes[i]]}\
+                 --num_preds 3 --spawn {episodes[i]} {'--command' if command else ''}")
